d6ae4b1d09998bb103468a4440ff7cb2416427db
Ayushi Gupta <Ayushi.Gupta@geminisolutions.com>
2023-06-20 15:36:57 +0530

    Navbar Component

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import './Navbar.css';
+const Navbar=()=>{
+    return(
+        <>
+        <div className='app_navbar'>
+           <h2>Kanban Board</h2>
+        </div>
+        </>
+    )
+}
+export default Navbar;
